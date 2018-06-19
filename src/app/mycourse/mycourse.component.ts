f40eb62b5cdce738b1e2bcebf0bd71750a1c505f
Andy <andrewpeliza@hotmail.com>
2018-06-19 17:02:31 +0100

    Sorting the mycourse component and course component

--- src/app/mycourse/mycourse.component.ts
+++ src/app/mycourse/mycourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mycourse',
@@ -7,24 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MycourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
   onClickAdv() {
+    // window.location.href = 'courses-selection';
+    // window.open('http://localhost:4200/courses-selection');
+    this.router.navigate(['/courses-selection']);
     console.log('Adv');
   }
 
   onClickEng() {
+    this.router.navigate(['/courses-selection']);
     console.log('eng');
   }
 
   onClickProject() {
+    this.router.navigate(['/courses-selection']);
     console.log('project');
   }
 
   onClickIs() {
+    this.router.navigate(['/courses-selection']);
     console.log('IS');
   }
 
